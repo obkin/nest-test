f5e084098e3ef1fab24c82a9443796c994027de3
obkin <kapellwork@gmail.com>
2025-01-10 17:57:19 +0200

    added: Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  configureSwagger(app);
+
+  await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
 bootstrap();
+
+function configureSwagger(app) {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest-Test')
+    .setDescription('Test-Task')
+    .setVersion('1.0.0')
+    .addTag('NestJS, MySQL (Sequelize), RestAPI, Docker')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api/docs', app, document);
+}
