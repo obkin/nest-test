461f0c66580a18c6d76e8072e09c13ea55583bae
obkin <kapellwork@gmail.com>
2025-01-10 18:00:38 +0200

    added: ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthModule, PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `./config/.env`,
+      isGlobal: true,
+    }),
+    UsersModule,
+    AuthModule,
+    PostsModule,
+  ],
   controllers: [],
   providers: [],
 })
